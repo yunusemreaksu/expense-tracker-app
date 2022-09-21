86eda90bff65bde468bdf5b8761fd197406b8328
Yunus Emre Aksu <emre_aksu@live.com>
2022-09-21 11:11:07 +0300

    Added header button and some styling

--- App.jsx
+++ App.jsx
@@ -7,6 +7,7 @@ import { RecentExpenses } from "./screens/RecentExpenses";
 import { AllExpenses } from "./screens/AllExpenses";
 import { GlobalStyles } from "./constants/styles";
 import { Ionicons } from "@expo/vector-icons";
+import { IconButton } from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 const BottonTabs = createBottomTabNavigator();
@@ -19,6 +20,14 @@ function ExpensesOverview() {
         headerTintColor: "white",
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {}}
+          />
+        ),
       }}
     >
       <BottonTabs.Screen
--- components/ExpensesOutput/ExpenseItem.jsx
+++ components/ExpensesOutput/ExpenseItem.jsx
@@ -3,8 +3,13 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
 export function ExpenseItem({ description, amount, date }) {
+  function expensePressHandler() {}
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>
@@ -21,6 +26,9 @@ export function ExpenseItem({ description, amount, date }) {
 }
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
